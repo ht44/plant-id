43d9b125ebc24be176a1bd41e1b0714fa93462be
James Proett <jamesproett@JAP.local>
2017-06-01 16:13:47 -0500

    gets and adds json to database

--- public/js/console/console.component.js
+++ public/js/console/console.component.js
@@ -1,64 +1,87 @@
 (function() {
-  'use strict'
+    'use strict'
 
-  angular.module('app').component('console', {
-      require: {layout: '^app'},
-      templateUrl: '/js/console/console.template.html',
-      controller: controller
+    angular.module('app').component('console', {
+        require: {
+            layout: '^app'
+        },
+        templateUrl: '/js/console/console.template.html',
+        controller: controller
     });
 
     controller.$inject = ['$http', '$stateParams', '$state']
+
     function controller($http, $stateParams, $state) {
-    this.$onInit = () => {
-      this.response = undefined;
-      console.log('initttt');
-    }
-    this.displayed = true;
-
-    this.togglePost = () => {
-        this.displayed = !this.displayed;
-    };
-    this.uploadFile = () => {
-
-      this.togglePost()
-      const xhr = new XMLHttpRequest();
-      const formData = new FormData();
-      const file = document.getElementById('file').files[0];
-      formData.append('file', file);
-      xhr.open('POST', '/api/store');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            handleResponse(xhr.response);
-          }
+        const vm = this;
+        this.$onInit = () => {
+            // this.response = undefined;
+            // this.responseObj = undefined;
+            this.parsedRes = undefined;
+            console.log('initttt');
         }
-      };
+        this.displayed = true;
 
-      xhr.send(formData);
-      return false;
-    };
+        this.togglePost = () => {
+            this.displayed = !this.displayed;
+        };
+        this.uploadFile = () => {
+            this.togglePost()
+            const xhr = new XMLHttpRequest();
+            const formData = new FormData();
+            const file = document.getElementById('file').files[0];
+            // const lat = this.responseObj.coordinates[0];
+            // const lng = this.responseObj.coordinates[1];
+            // const confidence = this.responseObj.confidence;
+            // const resHead = this.responseObj;
+            // console.log(JSON.parse(resHead));
+            // const species = resHead;
+            let lat = this.parsedRes.coordinates[0];
+            let lng = this.parsedRes.coordinates[1];
+            let confidence = this.parsedRes.confidence;
+            let name = this.parsedRes.properties.name;
+            // console.log(this.parsedRes.properties.head.common);
+            // console.log(this.parsedRes.confidence);
+            // console.log(this.parsedRes.coordinates);
+            formData.append('file', file);
+            formData.append('lat', lat);
+            formData.append('lng', lng);
+            formData.append('confidence', confidence);
+            formData.append('name', name);
+            xhr.open('POST', '/api/store');
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        handleResponse(xhr.response);
+                    }
+                }
+            };
 
-    this.submitFile = () => {
-    this.togglePost()
-      const xhr = new XMLHttpRequest();
-      const formData = new FormData();
-      const file = document.getElementById('file').files[0];
-      formData.append('file', file);
-      xhr.open('POST', '/api/classify');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            handleResponse(xhr.response);
-          }
+            xhr.send(formData);
+            return false;
+        };
+
+        this.submitFile = () => {
+            this.togglePost()
+            const xhr = new XMLHttpRequest();
+            const formData = new FormData();
+            const file = document.getElementById('file').files[0];
+            formData.append('file', file);
+            xhr.open('POST', '/api/classify');
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        this.parsedRes = JSON.parse(xhr.response);
+                    }
+                }
+            };
+            xhr.send(formData);
+            return false;
+        };
+
+
+        function handleResponse(response) {
+            let parsedRes = JSON.parse(response);
+            console.log(parsedRes);
         }
-      };
-      xhr.send(formData);
-      return false;
-    };
-
-
-    function handleResponse(response){
-      console.log(response);
     }
-  }
 }());
--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,10 @@ const geoJson = require('../custom_modules/geotagging');
 ///////////////////////////////////////////////////////////////////////////////
 
 const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-const visual_recognition = new VisualRecognitionV3({api_key: process.env.API_KEY, version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20});
+const visual_recognition = new VisualRecognitionV3({
+    api_key: process.env.API_KEY,
+    version_date: VisualRecognitionV3.VERSION_DATE_2016_05_20
+});
 
 ///////////////////////////////////////////////////////////////////////////////
 // STORAGE
@@ -34,7 +37,9 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage: storage});
+const upload = multer({
+    storage: storage
+});
 
 ///////////////////////////////////////////////////////////////////////////////
 // DATABASE
@@ -65,8 +70,7 @@ cloudant = require('cloudant')(dbCredentials.url);
 cloudant.db.create(dbCredentials.dbName, (err, res) => {
     if (err)
         console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
-    }
-);
+});
 
 db_class = cloudant.use(dbCredentials.classDB);
 db_obs = cloudant.use(dbCredentials.obsDB);
@@ -82,35 +86,29 @@ router.post('/classify', upload.single('file'), (req, res) => {
     }
     const temp = {
         "custom_classes": 24,
-        "images": [
-            {
-                "classifiers": [
-                    {
-                        "classes": [
-                            {
-                                "class": "Ligustrum lucidum",
-                                "score": 0.992155
-                            }, {
-                                "class": "Ligustrum quihoui",
-                                "score": 0.664165
-                            }, {
-                                "class": "Melia azedarach",
-                                "score": 0.560582
-                            }, {
-                                "class": "Rapistrum rugosum",
-                                "score": 0.986212
-                            }, {
-                                "class": "Torilis arvensis",
-                                "score": 0.989952
-                            }
-                        ],
-                        "classifier_id": "TexasInvasives_190947980",
-                        "name": "Texas Invasives"
-                    }
-                ],
-                "image": "b8772d41b377800b9769ba4deb22b5921496212536439.jpeg"
-            }
-        ],
+        "images": [{
+            "classifiers": [{
+                "classes": [{
+                    "class": "Ligustrum lucidum",
+                    "score": 0.992155
+                }, {
+                    "class": "Ligustrum quihoui",
+                    "score": 0.664165
+                }, {
+                    "class": "Melia azedarach",
+                    "score": 0.560582
+                }, {
+                    "class": "Rapistrum rugosum",
+                    "score": 0.986212
+                }, {
+                    "class": "Torilis arvensis",
+                    "score": 0.989952
+                }],
+                "classifier_id": "TexasInvasives_190947980",
+                "name": "Texas Invasives"
+            }],
+            "image": "b8772d41b377800b9769ba4deb22b5921496212536439.jpeg"
+        }],
         "images_processed": 1
     }
 
@@ -120,57 +118,59 @@ router.post('/classify', upload.single('file'), (req, res) => {
             coordinates = geoJson.extractLatLng(data);
         }
         let match;
-        visual_recognition.classify(params, (error, results) => {
-            if (error) {
-                console.error(error);
-            } else {
-                match = util.calcMatch(results);
-                db_class.get('Pistacia_chinensis', (err, body) => {
-                    res.json({coordinates: coordinates, properties: body.data, confidence: match.score});
-                });
-            }
+        // visual_recognition.classify(params, (error, results) => {
+        // if (error) {
+        //     console.error(error);
+        // } else {
+        match = util.calcMatch(temp);
+        db_class.get('Pistacia_chinensis', (err, body) => {
+            res.json({
+                coordinates: coordinates,
+                properties: body.data,
+                confidence: match.score
+            });
         });
+        // }
+        // });
     }).catch((error) => {
         res.json(error);
-    });
-
-router.post('/store', upload.single('file'), (req, res) => {
-console.log('got thereeee');
-// console.log(req.file.path);
-let file = fs.createReadStream(req.file.path);
-let results;
-request({
-    url: `https://dal.objectstorage.open.softlayer.com/v1/AUTH_7defd160d60c4e43b5f9dd6691e7e1a0/images/${req.file.filename}`,
-    method: 'PUT',
-    headers: {
-        'X-Auth-Token': req.app.get('storageToken'),
-        'Content-type': 'application/octet-stream',
-        'Content-length': req.file.size
-    },
-    body: file
-}, (err, response) => {
-    console.log(response.headers);
+    })
 });
 
-db_test.insert({
-    "type": "Feature",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [31.0, 90]
-    },
-    "properties": {
-        'date': new Date(),
-        'species': 'Ligustrum lucidum',
-        'confidence': 0.992155,
-        'valid_name': 'user_verified'
-    }
-}, function(err, body) {
-    console.log('working?');
-    if (!err)
-        console.log(body)
-})
+router.post('/store', upload.single('file'), (req, res) => {
+    console.log('got thereeee');
+    let file = fs.createReadStream(req.file.path);
+    let results;
+    request({
+        url: `https://dal.objectstorage.open.softlayer.com/v1/AUTH_7defd160d60c4e43b5f9dd6691e7e1a0/images/${req.file.filename}`,
+        method: 'PUT',
+        headers: {
+            'X-Auth-Token': req.app.get('storageToken'),
+            'Content-type': 'application/octet-stream',
+            'Content-length': req.file.size
+        },
+        body: file
+    }, (err, response) => {
+        console.log(err);
+    });
 
-res.json('image sent to storage')
+    db_test.insert({
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [req.body.lat, req.body.lng]
+        },
+        "properties": {
+            'date': new Date(),
+            'species': req.body.name,
+            'confidence': req.body.confidence,
+            'valid_name': 'user_verified'
+        }
+    }, function(err, body) {
+        console.log(err);
+        if (!err)
+            res.json(body);
+    })
 });
 
 ///////////////////////////////////////////////////////////////////////////////
