dbffb49d088451e1dc14603fb13664b685347acf
Hayden Turek <haydenturek@gmail.com>
2017-06-02 12:11:41 -0500

    stock plant

--- public/js/console/console.component.js
+++ public/js/console/console.component.js
@@ -13,16 +13,54 @@
     function controller($http, $stateParams, $state, $scope, $rootScope) {
         const vm = this;
         this.$onInit = () => {
-            this.response = undefined;
+            // this.response = undefined;
             this.payload = {
+              confidence: 99,
+              coordinates: [50, 50],
               properties: {
-                head: {
-                  name: "Triadica sebifera"
-                }
+                "head": {
+                  "name": "Triadica sebifera",
+                  "common": "Chinese tallow tree",
+                  "family": "Euphorbiaceae (Spurge Family)",
+                  "synonyms": [
+                    "Croton sebiferum", "Sapium sebiferum"
+                  ],
+                  "duration": "Perennial",
+                  "habit": "Tree",
+                  "listed_by": ["TDA Noxious Weed", "Invasive Plant Atlas of the US"]
+                },
+                "description": {
+                  "main": "Deciduous tree to 60 feet (18 m) in height and 3 feet (90 cm) in diameter, with ovalish- to rhomboid-shaped leaves, dangling yellowish spikes in spring yielding small clusters of three-lobed fruit that split to reveal popcorn-like seeds in fall and winter.",
+                  "ecological_threat": "Chinese tallow will transform native habitats into monospecific (single species) tallow forests in the absence of land management practices. Chinese tallow alters light availability for other plant species. Fallen tallow leaves contain toxins that create unfavorable soil conditions for native plant species. Chinese tallow will outcompete native plant species, reducing habitat for wildlife as well as forage areas for livestock.",
+                  "biology_and_spread": "Can reach reproductive age in as little as three years and prolifically produces seeds, which are readily transported by water and birds. Flowers mature March through May and fruit ripens August through November. Also propagates via cuttings, stumps, and roots.",
+                  "history": "Chinese tallowtree is native to China and Japan. It was introduced into the United States in the 1700?s in South Carolina. It was distributed in the Gulf Coast in the 1900?s by the U.S. Department of Agriculture in an attempt to establish a soap making industry.",
+                  "us_habitat": "Invades stream banks, riverbanks, and wet areas like ditches as well as upland sites. Thrives in both freshwater and saline soils. Shade tolerant, flood tolerant, and allelopathic. Increasing widely through ornamental plantings. Spreading by bird- and water-dispersed seeds and colonizing by prolific surface root sprouts."
+                },
+                "distribution": {
+                  "us_nativity": "Introduced to U.S.",
+                  "native_origin": "Temp. Asia-China & Taiwan (Germplasm Resources Information Network); Kartesz, J.T., and C.A. Meacham. Synthesis of the North American Flora, Version 1.0.",
+                  "us_present": "AL, AR, FL, GA, LA, MS, NC, SC, TX",
+                  "distribution": "Current distribution includes all of the Southeastern United States from Texas to Florida, North Carolina to Arkansas, and it was recently discovered in California."
+                },
+                "resembles": {
+                  "main": "",
+                  "alternatives": [
+                    "Cercis canadensis (eastern redbud)",
+                    "Cercis canadensis var. mexicana (Mexican redbud)",
+                    "Cercis canadensis var. texensis (Texas redbud)",
+                    "Acer grandidentatum (bigtooth maple)",
+                    "Acer negundo (boxelder)",
+                    "Ulmus crassifolia (cedar elm)"
+                  ]
+                },
+                "management": {
+                  "main": "Apply a triclopyr herbicide to basal bark in late summer or early fall (such as 20% Garlon 4 in oil) or, for large trees, apply directly to the stump after cutting down the tree (use Rodeo for trees growing in water). Pull up seedlings by hand. Large land areas can be managed by mowing and the careful use of controlled burns."
+                },
+                "text_references": [],
+                "online_resources": ["Miller, J.H. 2003. Nonnative invasive plants of southern forests: a field guide for identification and control. Gen. Tech. Rep. SRS-62. Asheville, NC: U.S. Department of Agriculture, Forest Service, Southern Research Station. 93 pp (USDA SRS).", "The Quiet Invasion:A Guide to Invasive Plants of the Galveston Bay Area"]
               }
             };
             this.displayed = true;
-            // console.log('initttt');
         }
 
         this.togglePost = () => {
@@ -43,7 +81,6 @@
           xhr.onreadystatechange = () => {
             if (xhr.readyState == 4) {
               if (xhr.status == 200) {
-                console.log('something');
                 let payload = JSON.parse(xhr.response);
                 this.payload = payload;
                 // console.log(payload);
@@ -63,7 +100,6 @@
             const formData = new FormData();
             const file = document.getElementById('file').files[0];
             document.getElementById('file').value = null;
-            console.log('WINNNNNNNN');
             if (this.payload.coordinates) {
               let lat = this.payload.coordinates[0];
               let lng = this.payload.coordinates[1];
@@ -91,10 +127,5 @@
             xhr.send(formData);
             return false;
         };
-
-
-        function handleResponse(response) {
-            console.log(response);
-        }
     }
 }());
